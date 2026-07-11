daae5197853a7b4b85321708f46809e930f551c4
KailasMetcalfe <kailasmetz@gmail.com>
2026-07-11 17:27:01 +0100

    Add basic HTML display

--- js.js
+++ js.js
@@ -39,10 +39,9 @@ const gameBoard = (() => {
 
 
 const gameController = (() => {
-    const player1 = new Player("player1", "X");
-    const player2 = new Player("player2", "O");
-    let isEnd = false;
-    let currentPlayer = player1;
+    const playerX = new Player("player1", "X");
+    const playerO = new Player("player2", "O");
+    let currentPlayer = playerX;
 
 
     function playRound(row, column) {
@@ -52,11 +51,10 @@ const gameController = (() => {
         }
         // If successful, continue with logic
         else {
-            checkWin()
-
-            if (isEnd) { console.log(`WINNER: ${currentPlayer.name}`); }
+            if (checkWin()) {console.log(`WINNER: ${currentPlayer.name}`);}
+            else if (checkTie()) {console.log("TIE");}
             else {
-                currentPlayer === player1 ? currentPlayer = player2 : currentPlayer = player1;
+                currentPlayer === playerX ? currentPlayer = playerO : currentPlayer = playerX;
             }
         }
     }
@@ -69,21 +67,54 @@ const gameController = (() => {
         for (let i = 0; i < 3; i++) {
             // Checks if row is equal
             if (allEqual(gameBoard.getRow(i)) && 
-            ["X", "O"].includes(board[i][0])) { isEnd = true; }
+            ["X", "O"].includes(board[i][0])) {return true;}
 
             // Checks if column is equal
             if (allEqual(gameBoard.getColumn(i)) && 
-            ["X", "O"].includes(board[0][i])) {isEnd = true; }
+            ["X", "O"].includes(board[0][i])) {return true;}
         }
 
         // Checks if leading diagonal (/) is equal
         if (allEqual([board[0][0], board[1][1], board[2][2]]) && 
-            ["X", "O"].includes(board[0][0])) {isEnd = true; }
+            ["X", "O"].includes(board[0][0])) {return true;}
 
         // Checks if antidiagonal (\) is equal
         if (allEqual([board[0][2], board[1][1], board[2][0]]) && 
-            ["X", "O"].includes(board[0][2])) {isEnd = true; }
+            ["X", "O"].includes(board[0][2])) {return true;}
+
+        return false;
     }
 
+    function checkTie() {
+        const board = gameBoard.getBoard();a
+        let arr = [];
+        for (let i = 0; i < 3; i++) {
+            for (let j = 0; j < 3; j++) {
+                arr.push(board[i][j]);
+            }
+        }
+        if (allEqual(arr) && ["X", "O"].includes(arr[0])) {return true;}
+        return false;
+    }
     return {playRound, getCurrentPlayer};
 })();
+
+
+const displayController = (() => {
+
+    function updateGridDisplay() {
+
+    }
+
+    function displayInfo() {
+
+    }
+
+    function setupEventListeners() {
+
+    }
+
+    function retrievePlayerNames() {
+        
+    }
+})
